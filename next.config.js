5001992a2745c104ec55d560ba9baf12600faf64
Zhihao Ni <xdnizhihao@gmail.com>
2024-11-20 17:19:58 -0800

    add stuff

--- next.config.js
+++ next.config.js
@@ -5,4 +5,32 @@ const withNextra = require('nextra')({
 
 module.exports = withNextra({
   basePath: '/help',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'logos-world.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'makeform.ai',
+      },
+    ],
+  },
 })
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,95 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { useConfig } from 'nextra-theme-docs'
 
-const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+const themeConfig = {
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/nilveryboring/makeform.github.io',
   },
-  chat: {
-    link: 'https://discord.com',
+  docsRepositoryBase:
+    'https://github.com/nilveryboring/makeform.github.io/blob/main',
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Makeform',
+    }
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: () => {
+    return (
+      <>
+        <Image
+          src="https://makeform.ai/icon.png"
+          alt="Makeform Logo"
+          className="inline-block h-8 w-8 hover:rotate-[720deg] transition-all duration-500 ease-in-out"
+          width={32}
+          height={32}
+        />
+        <span className="ml-2 font-extrabold hidden md:inline select-none">
+          Makeform AI Help Center
+        </span>
+      </>
+    )
+  },
+  head: () => {
+    const { frontMatter, title } = useConfig()
+    const ogTitle = title ? `${title} – Makeform` : 'Makeform - Form Builder'
+    const ogDescription = frontMatter.description || 'Build forms with AI'
+
+    return (
+      <>
+        <meta name="description" content={ogDescription} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@makeformAI" />
+        <meta property="og:title" content={ogTitle} />
+        <meta property="og:description" content={ogDescription} />
+      </>
+    )
+  },
   footer: {
-    text: 'Nextra Docs Template',
+    text: () => {
+      return (
+        <div className="flex items-center gap-2">
+          <span>© {new Date().getFullYear()} Makeform AI</span>
+        </div>
+      )
+    },
+  },
+  gitTimestamp({ timestamp }) {
+    return (
+      <>
+        Last updated on{' '}
+        <time dateTime={timestamp.toISOString()}>
+          {timestamp.toLocaleDateString('en-US', {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+          })}
+        </time>
+      </>
+    )
+  },
+  primaryHue: {
+    dark: 204,
+    light: 204,
+  },
+  darkMode: true,
+  nextThemes: {
+    defaultTheme: 'dark',
+  },
+  defaultMenuCollapseLevel: 1,
+  navigation: {
+    prev: true,
+    next: true,
+  },
+  banner: {
+    key: '1.0',
+    dismissible: true,
+    text: (
+      <a href="https://makeform.ai" target="_blank">
+        🎉 Makeform is now available! Read more →
+      </a>
+    ),
   },
 }
 
-export default config
+export default themeConfig
