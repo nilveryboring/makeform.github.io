8883332497cba5e3858ac74f8b3cdc4fbd2e5b62
Zhihao Ni <xdnizhihao@gmail.com>
2024-11-22 11:50:49 -0800

    win

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import '../styles.css'
 import { GoogleAnalytics } from '@next/third-parties/google'
-// make it safer
-const gaId = process.env.GA_ID || ''
 
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <GoogleAnalytics gaId={gaId} />
+      <GoogleAnalytics gaId={'G-5QNPN82FR3'} />
     </>
   )
 }
