f75c83158275e742ef7438af89632b7b283ec44f
Zhihao Ni <xdnizhihao@gmail.com>
2024-11-22 10:15:00 -0800

    add google analytics tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import '../styles.css'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-5QNPN82FR3" />
     </>
   )
 }
