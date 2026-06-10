b6579a61e6e6e439f4ca1eface21fa26de090081
Amengka <wang.zitin@northeastern.edu>
2026-06-09 20:12:06 -0700

    docs: add embed form documentation

--- pages/developer/_meta.ts
+++ pages/developer/_meta.ts
@@ -1,3 +1,3 @@
 export default {
-  embedded: "Embed Forms"
+  embedded: "Embed SDK"
 }
--- pages/developer/embedded/_meta.ts
+++ pages/developer/embedded/_meta.ts
@@ -1,7 +1,4 @@
 export default {
-  "embed-your-form": "Embed Your Form",
-  "popup-and-slider": "Popup & Slider Forms",
-  "embed-hidden-fields": "Hidden Fields in Embeds",
   "form-event": "Handle Form Events",
   "developer-resources": "Developer Resources"
 }
